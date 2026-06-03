de721b757ef715143539be9bc596fa874cb65fb9
Neal Fennimore <neal@neal.codes>
2026-06-02 21:17:37 -0400

    Fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,10 @@ pushd test
 
 [[ -f "nixos.qcow2" ]] && rm "nixos.qcow2"
 
-nix build .#nixosConfigurations.base.config.formats.vm
+nixos-rebuild build-vm --flake .#base
 
 export QEMU_OPTS="-device usb-host,vendorid=0x04e6,productid=0x581d -net user,hostfwd=tcp::2222-:22"
 
-./result/run-nixos-vm
+./result/bin/run-nixos-vm
 
 popd
